a99bc1e2c262b766e69f9a4170d359bb917617eb
Dean Kleissas <dean@gigantum.io>
2017-12-14 15:20:54 -0500

    remove windows conditionals in entrypoint.sh files

--- gtmlib/resources/labmanager_resources/entrypoint.sh
+++ gtmlib/resources/labmanager_resources/entrypoint.sh
@@ -13,32 +13,27 @@ chmod 777 /var/run/docker.sock
 # Add local user
 # Either use the LOCAL_USER_ID if passed in at runtime or
 # fallback
-if [ -z "$WINDOWS_HOST" ]; then
-    USER_ID=${LOCAL_USER_ID:-9001}
+USER_ID=${LOCAL_USER_ID:-9001}
 
-    echo "Starting with UID: $USER_ID"
-    useradd --shell /bin/bash -u $USER_ID -o -c "" -m giguser
-    export HOME=/home/giguser
+echo "Starting with UID: $USER_ID"
+useradd --shell /bin/bash -u $USER_ID -o -c "" -m giguser
+export HOME=/home/giguser
 
-    # Set permissions for container-container share
-    chown -R giguser:root /mnt/share/
+# Set permissions for container-container share
+chown -R giguser:root /mnt/share/
 
-    # Setup everything to allow giguser to run nginx and git
-    chown -R giguser:root /opt/log
-    chown -R giguser:root /opt/nginx
-    chown -R giguser:root /opt/redis
-    chown -R giguser:root /opt/run
-    chown -R giguser:root /var/lib/nginx/
-    chown -R giguser:root /var/log/nginx/
-    chown giguser:root /var/lock/nginx.lock
-    chown giguser:root /run/nginx.pid
-    chown giguser:root /var/run/docker.sock
-    cp /root/.gitconfig /home/giguser/
+# Setup everything to allow giguser to run nginx and git
+chown -R giguser:root /opt/log
+chown -R giguser:root /opt/nginx
+chown -R giguser:root /opt/redis
+chown -R giguser:root /opt/run
+chown -R giguser:root /var/lib/nginx/
+chown -R giguser:root /var/log/nginx/
+chown giguser:root /var/lock/nginx.lock
+chown giguser:root /run/nginx.pid
+chown giguser:root /var/run/docker.sock
+cp /root/.gitconfig /home/giguser/
 
-    # Not a windows host
-    exec gosu giguser "$@"
-else
-    # A windows host
-    exec "$@"
-fi
+# Not a windows host
+exec gosu giguser "$@"
 
